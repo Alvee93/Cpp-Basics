aefb8ef4b025206257effc3d8e11476c9e9c423c
Alvee93 <31181902+Alvee93@users.noreply.github.com>
2019-11-03 11:10:24 -0400

    Show all 25 shifts to Caesar Cipher
    The blank space, commas and inverted commas are opted out from the shifting operation.

--- character_shift_V0.1/main.cpp
+++ character_shift_V0.1/main.cpp
@@ -3,29 +3,31 @@
 using namespace std;
 int main()
 {
-  string c_text = "DRO MSDI LBSWC GSDR CEWWOB'C NOVSQRDC, GSDR MYVYBPEV ZBYNEMO SX DRO WKBUOD CDKXNC KXN RKGKSSKX WECSM CZSVVSXQ YXDY LOKMROC";
-  //cout << "Please enter the cipher text: ";
-  //getline (cin, c_text);
-  //cout << c_text.length() << endl;
-  
-  for (int i = 0; i < c_text.length(); i++) {
-      int x = (int) c_text[i];
-      //cout << x << endl;
-      if ( x>=75) {
-          x = x-10;
-          cout << (char) x;
-      }
-      else if (x==32) {
-          cout << " ";
-      }
-      else if (x==39) {
-          cout << "'";
-      }
-      else if (x==44) {
-          cout << ",";
-      }
-      else {
-          cout << (char) (x +16);
-      }
-  }
+    string c_text = "DRO MSDI LBSWC GSDR CEWWOB'C NOVSQRDC, GSDR MYVYBPEV ZBYNEMO SX DRO WKBUOD CDKXNC KXN RKGKSSKX WECSM CZSVVSXQ YXDY LOKMROC";
+     
+    for (int shift=0; shift<26; shift++ ) {
+        
+        for (int i = 0; i < c_text.length(); i++) {
+            
+            int x = (int) c_text[i];
+            //cout << x << endl;
+            if ( x>=(90-shift+1)) {
+                x = x-(26-shift);
+                cout << (char) x;
+            }
+            else if (x==32) {
+                cout << " ";
+            }
+            else if (x==39) {
+                cout << "'";
+            }
+            else if (x==44) {
+                cout << ",";
+            }
+            else {
+                cout << (char) (x + shift);
+            }
+        }
+        cout << endl << "-----------------------------------------------------" << endl;
+    }
 }
